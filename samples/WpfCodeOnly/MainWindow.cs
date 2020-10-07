014f3bf770c6792a954bdcf27954b56d12f7edcb
clean-code-craft <cleancodcraft@gmail.com>
2020-10-07 15:32:39 +0530

    Code Only

---
+++ samples/WpfCodeOnly/MainWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfCodeOnly
+{
+    public class MainWindow:Window
+    {
+        public MainWindow()
+        {
+            this.Title = "MainWindow";
+        }
+    }
+}
---
+++ samples/WpfCodeOnly/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCodeOnly
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            System.Windows.Application _wpfApp = new System.Windows.Application();
+            MainWindow _window = new MainWindow();
+            _wpfApp.Run(_window);
+        }
+    }
+}
