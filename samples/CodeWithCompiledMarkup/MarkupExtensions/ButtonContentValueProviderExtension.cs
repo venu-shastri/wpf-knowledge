9ad3b611a698613405d8be535b58abd28ba5294d
clean-code-craft <cleancodcraft@gmail.com>
2020-10-07 17:00:47 +0530

    MarkupExtensions

---
+++ samples/CodeWithCompiledMarkup/MarkupExtensions/ButtonContentValueProviderExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace CodeWithCompiledMarkup.MarkupExtensions
+{
+    public class ButtonContentValueProviderExtension:MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            Utility.ButtonContentProvider _provider = new Utility.ButtonContentProvider();
+            return _provider.GetButtonContentText();
+        }
+    }
+}
