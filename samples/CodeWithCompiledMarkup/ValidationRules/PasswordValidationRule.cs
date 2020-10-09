b032a5f5691d249b78af76ce469a59b8edb09712
clean-code-craft <cleancodcraft@gmail.com>
2020-10-09 18:01:06 +0530

    Error Style

---
+++ samples/CodeWithCompiledMarkup/ValidationRules/PasswordValidationRule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CodeWithCompiledMarkup.ValidationRules
+{
+    public class PasswordValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string password = value as string;
+            if (string.IsNullOrEmpty(password))
+            {
+                return new ValidationResult(false, "Password Requires Value");
+            }
+            else
+            {
+                if(password.Length > 4 && password.Length < 8)
+                {
+                    return new ValidationResult(true, "");
+                    
+                }
+                return new ValidationResult(false, "Password Length > 4 and  < 8");
+
+            }
+
+        }
+    }
+}
