2caa915e77b9678e93b66cb341642028c4c00cdd
clean-code-craft <cleancodcraft@gmail.com>
2020-10-09 16:33:24 +0530

    Assignment

---
+++ samples/CodeWithCompiledMarkup/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWithCompiledMarkup
+{
+   public  class Calculator
+    {
+        int operandOne;
+        int operandTwo;
+        int result;
+
+        public void Add()
+        {
+            int temp = operandOne + operandTwo;
+        }
+        public void  Sub()
+        {
+            int temp = operandOne - operandTwo;
+        }
+
+        public void Clear()
+        {
+            //Clear All 
+        }
+    }
+}
